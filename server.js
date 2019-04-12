34d92ca9261790ad3de6a8df8d01c6c206cfc38f
Rebecca <rszeto@kensingtonhealth.org>
2019-04-12 16:28:01 -0400

    Serving from pages instead of dist, change if necessary to bulk up the site

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/pages"));
 var port = process.env.PORT || 7000;
 app.listen(port);
 console.log('server has been started '+ port);
