7718855e744d5f6f6fa1ff2419896e32a872f493
Rebecca <rszeto@kensingtonhealth.org>
2019-04-12 17:53:12 -0400

    Fixed the missing dist build

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/pages"));
-var port = process.env.PORT || 3000;
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server has been started '+ port);
-
+console.log('server started '+ port);
