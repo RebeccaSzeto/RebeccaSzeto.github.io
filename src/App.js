76fca5e15e0b46900987c43618f99417553dfc42
Rebecca Szeto <rebecca.szeto@agnicoeagle.com>
2020-01-07 10:38:57 -0500

    Change project to react

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to WASZ</h1>
+        <p>Exciting things will be coming your way. Stay Tuned        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          About
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
