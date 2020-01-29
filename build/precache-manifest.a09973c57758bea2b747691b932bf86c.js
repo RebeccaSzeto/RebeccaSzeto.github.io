477197dc43f0809ed1078783b60ec5063644d946
Rebecca Szeto <rebecca.szeto@agnicoeagle.com>
2020-01-29 17:14:11 -0500

    Added a build tool

---
+++ build/precache-manifest.a09973c57758bea2b747691b932bf86c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1bdb5cb6ae74fa78dedf3223c41f71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce67f4749e78c2a9659c",
+    "url": "/static/css/2.aeb2d6c0.chunk.css"
+  },
+  {
+    "revision": "0080e15fc4e9e91c00de",
+    "url": "/static/css/main.6488a6af.chunk.css"
+  },
+  {
+    "revision": "ce67f4749e78c2a9659c",
+    "url": "/static/js/2.bdbde236.chunk.js"
+  },
+  {
+    "revision": "e2b86f756adb8191050c99964c58c794",
+    "url": "/static/js/2.bdbde236.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0080e15fc4e9e91c00de",
+    "url": "/static/js/main.3faa55d8.chunk.js"
+  },
+  {
+    "revision": "1cdd64b61176c4a279c2",
+    "url": "/static/js/runtime-main.b015e80d.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
