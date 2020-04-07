976c817f5e881dfcda9cb2fef2175982bf800ed9
Rebecca Szeto <rebecca.szeto@agnicoeagle.com>
2020-04-07 19:32:30 -0400

    Modified to a relative path file

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 //app.use(express.static(path.join(__dirname, 'public')));
