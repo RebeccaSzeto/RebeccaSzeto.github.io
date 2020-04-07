d8cdd571a4c1166450ad0fed2e1d5d5600ce5b77
Rebecca Szeto <rebecca.szeto@agnicoeagle.com>
2020-04-07 18:55:27 -0400

    Built app using an express server

--- app/src/App.js
+++ app/src/App.js
@@ -3,14 +3,6 @@ import logo from './theor_logo.svg';
 import './App.css';
 import Details from './components/Details.js';
 import Sidebar from './components/Sidebar.js';
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = process.env.PORT || 3001
-
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.listen(port, () => console.log("Listening on Port", port)) 
 
 function App() {
   return (
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = process.env.PORT || 3001
-
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.listen(port, () => console.log("Listening on Port", port)) 
