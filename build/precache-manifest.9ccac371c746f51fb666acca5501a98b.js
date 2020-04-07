f74e8d2e2a603c1f3644e132d344ed45dcd58972
Rebecca Szeto <rebecca.szeto@agnicoeagle.com>
2020-04-07 19:33:13 -0400

    Added build files

---
+++ build/precache-manifest.9ccac371c746f51fb666acca5501a98b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382d27fc652419bc06c87fc332097835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc1c1ad79f4623c20a1b",
+    "url": "/static/css/main.04ce9723.chunk.css"
+  },
+  {
+    "revision": "6163ebeebbadea442a0e",
+    "url": "/static/js/2.9ffa436b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9ffa436b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1c1ad79f4623c20a1b",
+    "url": "/static/js/main.8cc9e926.chunk.js"
+  },
+  {
+    "revision": "dc0de9daa890586c93bc",
+    "url": "/static/js/runtime-main.0438ea87.js"
+  },
+  {
+    "revision": "54e0db158c0bc86e11576ec953fc2e48",
+    "url": "/static/media/theor_logo.54e0db15.svg"
+  }
+]);
